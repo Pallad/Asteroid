5782a4c9c3a98a7bae70b51d7e7652ad80e82653
pallad <coders.ru@gmail.com>
2023-06-20 22:34:12 +0300

    fixed files' paths

--- Asteroid.py
+++ Asteroid.py
@@ -4,19 +4,19 @@ import pygame
 from settings import SIZE, COLOR_BACKGROUND, COLOR_TRANSPARENT
 from game_objects import Player, Background, Plasma_bullet, Asteroid, BadaBoom
 
-def StartGame():
+
+def start_game():
     pygame.init()
     pygame.display.set_caption('TestGame')
     screen = pygame.display.set_mode(SIZE)
     clock = pygame.time.Clock()
 
-    #Group objects
+    # Group objects
     objects = pygame.sprite.Group()
     bullets = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
 
-
-    #Game objects
+    # Game objects
     player = Player(bullets, clock)
     background = Background()
 
@@ -30,15 +30,15 @@ def StartGame():
             if event.type == pygame.QUIT:
                 active = False
 
-        #Respoun asteroid
+        # Respoun asteroid
         Asteroid.respoun(asteroids, clock)
 
-        #Change state player
+        # Change state player
         objects.update()
         bullets.update()
         asteroids.update()
 
-        #Collise
+        # Collise
         pygame.sprite.groupcollide(bullets, asteroids, True, True)
 
         if pygame.sprite.spritecollide(player, asteroids, True):
@@ -48,22 +48,22 @@ def StartGame():
             pygame.time.delay(1000)
             active = False
 
-        #Fill background
+        # Fill background
         screen.fill(COLOR_BACKGROUND)
 
-        #Draw Background
+        # Draw Background
         background.draw(screen)
 
-        #Draw player
+        # Draw player
         player.draw(screen)
 
-        #Draw bullet
+        # Draw bullet
         bullets.draw(screen)
 
-        #Draw asteroid
+        # Draw asteroid
         asteroids.draw(screen)
 
-        #Show frame
+        # Show frame
         pygame.display.flip()
 
         # Delay game loop -- frame per second
@@ -72,6 +72,6 @@ def StartGame():
     pygame.quit()
     sys.exit(0)
 
-if __name__ == '__main__':
-    StartGame()
 
+if __name__ == '__main__':
+    start_game()
